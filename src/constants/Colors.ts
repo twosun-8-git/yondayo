723f740d6741eaef1dff0a289e1539b4243c1a61
Chinen Tsukasa <b_chinen.t@impl.co.jp>
2025-10-05 18:53:12 +0900

    backup

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -3,30 +3,51 @@ export const Colors = {
     900: "#444444",
   },
   blue: {
-    500: "#81D3F3",
-    700: "#60C6F0",
+    500: "#81d3f3",
+    700: "#60c6f0",
   },
   gray: {
     100: "#fafafa",
     200: "#eeeeee",
-    300: "#C6C6C6",
+    300: "#c6c6c6",
     500: "#999999",
-    700: "#666666",
+    700: "#777777",
+    900: "#666666",
   },
   red: {
-    700: "#FA7777",
+    700: "#fa7777",
   },
   yellow: {
-    700: "#FFD497",
+    700: "#ffd497",
   },
 };
 
+/**
+ * - Blue[500]: #81d3f3
+ * - Blue[700]: #60c6f0
+ */
 export const Blue = Colors.blue;
 
+/**
+ * - Yellow[700]: #ffd497
+ */
 export const Yellow = Colors.yellow;
 
+/**
+ * - Red[700]: #fa7777
+ */
 export const Red = Colors.red;
 
+/**
+ * - Black[900]: #444444
+ */
 export const Black = Colors.black;
 
+/**
+ * - Gray[100]: #fafafa
+ * - Gray[200]: #eeeeee
+ * - Gray[300]: #c6c6c6
+ * - Gray[500]: #999999
+ * - Gray[700]: #777777
+ * - Gray[900]: #666666 */
 export const Gray = Colors.gray;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,49 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        app: 14,
+        button: 44,
+      },
+      boxShadow: {
+        original: "0 0 2px 0 rgba(0, 0, 0, 0.15)",
+      },
+      fontFamily: {
+        sans: ["System", "sans-serif"],
+      },
+      fontSize: {
+        xs: 8,
+        s: 10,
+        base: 14,
+        m: 16,
+        l: 20,
+        xl: 24,
+      },
+      colors: {
+        black: {
+          900: "#444444",
+        },
+        blue: {
+          500: "#81D3F3",
+          700: "#60C6F0",
+        },
+        gray: {
+          100: "#fafafa",
+          200: "#eeeeee",
+          300: "#C6C6C6",
+          500: "#999999",
+          700: "#666666",
+        },
+        red: {
+          700: "#FA7777",
+        },
+        yellow: {
+          700: "#FFD497",
+        },
+      },
+    },
   },
+
   plugins: [],
 };
